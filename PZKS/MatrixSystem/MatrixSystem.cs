551c69d2e25e22df2b66a95e1680111ade84fe4f
gazinaft <gazinaft@gmail.com>
2023-12-14 02:07:16 +0300

    lab 5 wip

--- PZKS/MatrixSystem/MatrixSystem.cs
+++ PZKS/MatrixSystem/MatrixSystem.cs
@@ -24,8 +24,8 @@ public class MatrixSystem
     {
         MatrixHosts = new Dictionary<int, List<ExecutionBlock>>
         {
-            { 1, new List<ExecutionBlock>() },
-            { 2, new List<ExecutionBlock>() }
+            { 1, Enumerable.Repeat(new ExecutionBlock{ State = SysState.Empty }, 1000).ToList() },
+            { 2, Enumerable.Repeat(new ExecutionBlock{ State = SysState.Empty }, 1000).ToList() }
         };
         _currentPointer = 0;
     }
@@ -41,18 +41,67 @@ public class MatrixSystem
     }
     
     // Calculate
-    public void ExecuteExpressionNodes(ExpressionNode? node1, ExpressionNode? node2)
+    public void ExecuteExpressionNode(ExpressionNode node1, int host)
+    {
+        node1.Calculate(host);
+    }
+
+    public void WriteOperationsToSystem(ExpressionNode? node1, ExpressionNode? node2)
     {
         if (node1 != null)
         {
-            node1.Calculate(1);
-        }
-        if (node2 != null)
-        {
-            node2.Calculate(2);
+            var cyclesToTransmit1 = CostOfOperation(node1, 1);
+            
         }
     }
 
+    private void WriteOneOperationToSystem(ExpressionNode node, int host)
+    {
+        UpdatePointer();
+        var cyclesToTransmit = CostOfOperation(node, host);
+        
+    }
+
+    private static int OtherHost(int host) => host == 1 ? 2 : 1;
+
+    private void ReadDataFromHost(int sourceHost)
+    {
+        
+    }
+
+    private int LastValidPositionAtHost(int host)
+    {
+        var counter = 0;
+        for (int i = MatrixHosts[host].Count - 1; i >= 0; i--)
+        {
+            if (MatrixHosts[host][i].State != SysState.Empty)
+            {
+                counter = i;
+                break;
+            }
+        }
+
+        return counter;
+    }
+    
+    private int UpdatePointer()
+    {
+        var len1 = MatrixHosts[1].Count;
+        var len2 = MatrixHosts[2].Count;
+        if (len1 == len2)
+        {
+            _currentPointer = len1;
+            return _currentPointer;
+        }
+
+        int maxLen = len1 > len2 ? len1 : len2;
+        int delta = Math.Abs(len1 - len2);
+        int minHost = len1 > len2 ? 2 : 1;
+
+        _currentPointer = maxLen;
+        return _currentPointer;
+    }
+    
     public float GetEffectivityCoef(int index)
     {
         if (!MatrixHosts.ContainsKey(index)) return 0;
@@ -75,4 +124,55 @@ public class MatrixSystem
     }
     
     
+    public List<ExpressionNode> GetNodesCanBeCalculated(ref ExpressionNode node)
+    {
+        var result = new List<ExpressionNode>();
+        if (node.CanBeCalculated())
+        {
+            if (node.IsCalculated)
+            {
+                return result;
+            }
+            result.Add(node);
+            return result;
+        }
+
+        for (var index = 0; index < node.Children.Count; index++)
+        {
+            var expressionNode = node.Children[index];
+            if (expressionNode.CanBeCalculated() && !expressionNode.IsCalculated)
+            {
+                result.Add(expressionNode);
+            }
+            else
+            {
+                result.AddRange(GetNodesCanBeCalculated(ref expressionNode));
+            }
+        }
+
+        return result;
+    }
+
+    private List<ExpressionNode> GetBestExpressionNodes(List<ExpressionNode> expressionNodes)
+    {
+        var result = new List<ExpressionNode>();
+        if (expressionNodes.Count == 0) return result;
+        var grouped = expressionNodes.GroupBy(x => x.NodeToken.TokenType);
+        var largestGroup = grouped.OrderByDescending(g => g.Count()).First().ToList();
+        if (largestGroup.Count == 1)
+        {
+            
+        }
+    }
+    
+    private float CostOfOperations(ExpressionNode node1, ExpressionNode node2)
+    {
+        return CostOfOperation(node1, 1) + CostOfOperation(node2, 2) - (node1.Parent == node2.Parent ? 0.5f : 0f);
+    }
+
+    private float CostOfOperation(ExpressionNode node, int host)
+    {
+        var NonCompatibleDependencies = node.Children.Count(x => (x.CalculatedSource & host) == 0);
+        return NonCompatibleDependencies;
+    }
 }
